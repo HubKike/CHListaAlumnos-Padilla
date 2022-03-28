ce819aa10d196a190a9a976ca8f101db202287f6
Pedro Padilla <ultrared808@gmail.com>
2022-03-28 17:26:08 -0600

    Actualizado con directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ import Alumnos_json from 'src/assets/json/alumnos.json'
 export class AppComponent {
   title = 'CHListaAlumnos-Padilla';
   listaAlumnos: any = Alumnos_json;
+
+  userClicked(username:string): void{
+    alert('El alumno ' + username + ' fue clickeado')
+  }
+
+  alumnosMayores = this.listaAlumnos.filter((i:any) => i.age >= 21);
+
 }
